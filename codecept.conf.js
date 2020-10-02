c44cb984849aca2922821e20b40997b28dd893a5
kurospe <akuntestspe@gmail.com>
2020-10-02 10:21:34 +0700

    first push

---
+++ codecept.conf.js
@@ -0,0 +1,38 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Appium: {
+      app: 'apk/pos-rewash.apk',
+      appPackage: 'spe.pos.rewash',
+      appActivity: 'spe.pos.rewash.feature.login.loginActivity',
+      platform: 'Android',
+      device: 'Android Emulator',
+      automationName: 'UiAutomator2'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'Automation Mobile',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
